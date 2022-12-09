cc7964ed716ff258d326ef469a3d7f4c76750c52
Lucas Maia <36086588+lucasmaia123@users.noreply.github.com>
2022-12-09 19:37:09 -0300

    Update plota_arestas2d.m

--- plota_arestas2d.m
+++ plota_arestas2d.m
@@ -1,7 +1,7 @@
-function plota_arestas2d(Vertices, Arestas)
+function plota_arestas2d(Vertices, Arestas, cor)
     hold on;
     for i = 1:length(Arestas)
-        plot(Vertices(1,Arestas(i,:)), Vertices(2,Arestas(i,:)), 'b-')
+        plot(Vertices(1,Arestas(i,:)), Vertices(2,Arestas(i,:)), cor)
     end
     xlabel('x');
     ylabel('y');
