7d9a5f7a15d76713d2ff818c60443969eeb8e3e9
Lucas Maia <36086588+lucasmaia123@users.noreply.github.com>
2022-12-09 18:36:54 -0300

    Delete Transform.m

--- Transform.m
+++
@@ -1,8 +0,0 @@
-function object = Transform(tipo, ind, fatores, world)
-    if ind == 0
-        if tipo == 1
-            for i = 1:length(world)
-                world(i).Escala(fatores)
-            else if tipo == 2
-                
-
