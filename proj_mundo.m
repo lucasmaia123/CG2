fc71db6a18c47aebacdcfbed286c88bfcc901f37
Lucas Maia <36086588+lucasmaia123@users.noreply.github.com>
2022-12-15 16:24:35 -0300

    Update proj_mundo.m

--- proj_mundo.m
+++ proj_mundo.m
@@ -1,13 +1,13 @@
 function world = proj_mundo(alfa, ratio, world)
-    d = cotd(alfa/2);
+    d = cotd(alfa/2); % Distância da camera para a tela
     for i = 1:length(world)
         world(i).render = false;
         origem = world(i).origem_proj;
-        %Checa se o objeto está no eixo z da camera
-        if origem(3) < -d && origem(3) > -20
-            %Checa se o objeto está no eixo y da camera
+        % Checa se o objeto está no eixo z da camera
+        if origem(3) < -d && origem(3) > -20 % distância de visão
+            % Checa se o objeto está no eixo y da camera
             if origem(2) <= -origem(3)/d && origem(2) >= origem(3)/d
-                %Checa se o objeto está no eixo x da camera
+                % Checa se o objeto está no eixo x da camera
                 if origem(1) <= -origem(3)*ratio/d && origem(1) >= origem(3)*ratio/d
                     world(i).render = true;
                     for j = 1:length(world(i).v_proj)
@@ -16,7 +16,7 @@ function world = proj_mundo(alfa, ratio, world)
                             0 d/z 0 0;
                             0 0 0 d;
                             0 0 0 1;
-                            ];
+                            ]; % Matriz de projeção sem o near/far (Não são necessários para este projeto)
                         world(i).v_proj(:,j) = T*world(i).v_proj(:,j);
                     end
                 end
